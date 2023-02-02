68d860bbc8759524181f9f0658c8d48aa7cfc3a4
MaxiSilva93 <42882958+MaxiSilva93@users.noreply.github.com>
2023-02-02 18:33:41 -0500

    Principal Commit
    Uploading app

---
+++ app.js
@@ -0,0 +1,16 @@
+const adviceId = document.getElementById('advice-id');
+const adviceTxt = document.getElementById('advice-text');
+const adviceBtn = document.getElementById('generator');
+const url = 'https://api.adviceslip.com/advice';
+
+async function getAdvice() {
+    const res = await fetch(url);
+    const { slip: {id , advice} } = await res.json();
+    console.log(res);
+    adviceId.innerText = id;
+    adviceTxt.innerText = advice;
+}
+
+getAdvice();
+
+adviceBtn.addEventListener('click',getAdvice);
